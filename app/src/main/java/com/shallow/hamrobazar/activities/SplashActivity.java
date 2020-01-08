8af95d2469fcbc700b0ca292bd235da689500383
sujitkhadgi <sujitkhadgi12@gmail.com>
2020-01-08 22:55:09 +0545

    UI of splash and welcome

---
+++ app/src/main/java/com/shallow/hamrobazar/activities/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.shallow.hamrobazar.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
---
+++ app/src/main/java/com/shallow/hamrobazar/activities/WelcomeActivity.java
@@ -0,0 +1,14 @@
+package com.shallow.hamrobazar.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
