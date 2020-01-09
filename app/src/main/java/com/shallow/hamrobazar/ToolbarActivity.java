329fd31fd1e149ebfb450c0c8e25bd191a1763cd
sujitkhadgi <sujitkhadgi12@gmail.com>
2020-01-09 13:15:54 +0545

    UI for login and dashboard

---
+++ app/src/main/java/com/shallow/hamrobazar/ToolbarActivity.java
@@ -0,0 +1,14 @@
+package com.shallow.hamrobazar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ToolbarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toolbar);
+    }
+}
---
+++ app/src/main/java/com/shallow/hamrobazar/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.shallow.hamrobazar.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.shallow.hamrobazar.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
